396ff94208834f9a38f0e86c3117a203f5b5ed7e
Tank.he <tank.he@greenpacket.com.cn>
2020-11-23 11:21:35 +0800

    modify composer

---
+++ src/Hello.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hehui
+ * Date: 2020/11/23
+ * Time: 上午11:01
+ */
+namespace Leesin;
+
+class Hello
+{
+    public static function SayHello(){
+        return 'hello world';
+    }
+}
---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hehui
+ * Date: 2020/11/23
+ * Time: 上午11:05
+ */
+require __DIR__.'/vendor/autoload.php';
+
+use Leesin\Hello;
+
+echo Hello::SayHello();
